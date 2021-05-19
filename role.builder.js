bbbd428c8f10fe9c8bfba578a2efa44bb9d56d65
Shiba <beast14425@gmail.com>
2021-05-19 16:15:25 -0400

    Updated to allow energy overflow and placement into containers, as well as repairing these containers.

--- role.builder.js
+++ role.builder.js
@@ -15,17 +15,31 @@ var roleBuilder = {
 	    if(creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
 			creep.memory.storing = false;
+			creep.memory.repairing = false;
             creep.say('🔄 harvest');
 	    }
+		else if (creep.store.getFreeCapacity() == 0 && creep.room.find(FIND_MY_STRUCTURES).hits <= creep.room.find(FIND_MY_STRUCTURES).hitsMax - 20000,{
+			filter: {
+				structureType: STRUCTURE_CONTAINER
+			}
+		})
+		{
+			creep.memory.building = false;
+			creep.memory.storing = false;
+			creep.memory.repairing = true;
+			creep.say(' repair');
+		}
 	    else if(creep.store.getFreeCapacity() == 0 && creep.room.find(FIND_MY_CONSTRUCTION_SITES).length != 0) {
 	        creep.memory.building = true;
 			creep.memory.storing = false;
+			creep.memory.repairing = false;
 	        creep.say('🚧 build');
 	    }
 		else if (creep.store.getFreeCapacity() == 0)
 		{
 			creep.memory.building = false;
 			creep.memory.storing = true;
+			creep.memory.repairing = false;
 			creep.say(' store');
 		}
 
@@ -48,6 +62,20 @@ var roleBuilder = {
 			});
 			if(targets.length > 0) {
 				if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+				}
+			}
+		}
+		else if (creep.memory.repairing)
+		{
+			var targets = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_CONTAINER) &&
+						structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+				}
+			});
+			if(targets.length > 0) {
+				if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
 				}
 			}
