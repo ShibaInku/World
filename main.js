71e059da013edc2a0dff50a283602607baff17f3
Shiba <beast14425@gmail.com>
2021-05-20 01:32:47 -0400

    screep names broke this

--- main.js
+++ main.js
@@ -21,19 +21,19 @@ module.exports.loop = function () {
 
     //Spawning each role according to spawn caps
     if(harvesters.length < 5) {
-        var newName = 'Harvester' + Math.round(Game.time / 10000);
+        var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'harvester'}});
     }
     if(upgraders.length < 5) {
-        var newName = 'Upgrader' + Math.round(Game.time / 10000);
+        var newName = 'Upgrader' + Game.time;
         console.log('Spawning new upgrader: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'upgrader'}});
     }
     if(builders.length < 5) {
-        var newName = 'Builder' + Math.round(Game.time / 10000);
+        var newName = 'Builder' + Game.time;
         console.log('Spawning new builder: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE], newName,
             {memory: {role: 'builder'}});
